aadab896a13ef8c9a036dd4a325c77352ad548ea
suhas018 <92872637+suhas018@users.noreply.github.com>
2024-04-11 16:07:47 +0530

    Add files via upload

---
+++ restaurant/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('book/', views.book, name="book"),
+    # ntg
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/',views.display_menu_item, name="menu_item"),
+]
